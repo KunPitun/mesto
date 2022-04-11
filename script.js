bbf0133cbb71c05a2b61b7ccece841a722fdc40a
Nikolaenko Nikolay <nickynick29a@gmail.com>
2022-04-11 20:56:21 +0300

    For merge

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ function popupOpenClose() {
   popupInputInfo.value = '';
 }
 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  popupInputName = document.querySelector('.popup__input_type_name');
+  popupInputInfo = document.querySelector('.popup__input_type_user-info');
+  userName.textContent = popupInputName.value;
+  userInfo.textContent = popupInputInfo.value;
+  popupInputName.setAttribute('placeholder', popupInputName.value);
+  popupInputInfo.setAttribute('placeholder', popupInputInfo.value);
+  popupOpenClose();
+}
+
 let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
 let popupInputName = document.querySelector('.popup__input_type_name');
 let popupInputInfo = document.querySelector('.popup__input_type_user-info');
-let editBtn = document.querySelector('.profile__edit-btn');
-let closeBtn = document.querySelector('.popup__close-btn');
 let saveBtn = document.querySelector('.popup__save-btn');
+let closeBtn = document.querySelector('.popup__close-btn');
+let userName = document.querySelector('.profile__user-name');
+let userInfo = document.querySelector('.profile__user-info');
+let editBtn = document.querySelector('.profile__edit-btn');
 
 editBtn.addEventListener('click', popupOpenClose);
 closeBtn.addEventListener('click', popupOpenClose);
+formElement.addEventListener('submit', formSubmitHandler);

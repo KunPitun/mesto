26516f3ec352866972919d397b249e7eda9117a9
Nikolaenko Nikolay <nickynick29a@gmail.com>
2022-04-16 01:06:44 +0300

    fix: review fixes

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-function popupOpenClose() {
-  popup.classList.toggle('popup_closed');
-  popupInputName.value = '';
-  popupInputInfo.value = '';
-}
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  popupInputName = document.querySelector('.popup__input_type_name');
-  popupInputInfo = document.querySelector('.popup__input_type_user-info');
-  userName.textContent = popupInputName.value;
-  userInfo.textContent = popupInputInfo.value;
-  popupInputName.setAttribute('placeholder', popupInputName.value);
-  popupInputInfo.setAttribute('placeholder', popupInputInfo.value);
-  popupOpenClose();
-}
-
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let popupInputName = document.querySelector('.popup__input_type_name');
 let popupInputInfo = document.querySelector('.popup__input_type_user-info');
-let saveBtn = document.querySelector('.popup__save-btn');
 let closeBtn = document.querySelector('.popup__close-btn');
 let userName = document.querySelector('.profile__user-name');
 let userInfo = document.querySelector('.profile__user-info');
 let editBtn = document.querySelector('.profile__edit-btn');
+
+function popupOpenClose() {
+  popup.classList.toggle('popup_closed');
+  popupInputName.value = userName.textContent;
+  popupInputInfo.value = userInfo.textContent;
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  userName.textContent = popupInputName.value;
+  userInfo.textContent = popupInputInfo.value;
+  popupOpenClose();
+}
 
 editBtn.addEventListener('click', popupOpenClose);
 closeBtn.addEventListener('click', popupOpenClose);
